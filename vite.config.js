7a6f4078d4e1c0ec8922b55b6a25b16b39353312
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-10 09:57:48 +0530

    added vite.config and updated package.json

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.jsx'),
+      name: 'OpenIMISFeCalculation',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`
+    },
+    sourcemap: true,
+    outDir: 'dist',
+    rollupOptions: {
+      external: [
+        /^@babel.*/,
+        /^@date-io\/.*/,
+        /^@material-ui\/.*/,
+        /^@openimis.*/,
+        'classnames',
+        'clsx',
+        'history',
+        /^lodash.*/,
+        'moment',
+        'prop-types',
+        /^react.*/,
+        /^redux.*/,
+        'hot-formula-parser'
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+})
